b4afd8c6f6945f4f67cff925bf560763dd2bf009
Saad Ahmed <saamed9198@sbtjapan.com>
2023-03-14 13:47:48 +0500

    NOT logical Operator

---
+++ js/18-NOT-logical-operator/index.js
@@ -0,0 +1,23 @@
+// ! Not logical Operator
+// typical used to reverse a condtion's booleans value
+// true -> false
+// false -> true
+
+let temp = 15;
+let sunny = false;
+
+if(!(temp > 0)){
+    console.log("It's cold outside ");
+}
+else{
+    console.log("It's Warm outside");
+}
+
+if(!sunny) {
+    console.log("its cloudy outside")
+}
+else{
+    console.log("its sunny  outside")
+}
+
+    

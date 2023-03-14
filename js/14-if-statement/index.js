f751bf5255038b795d4f849297bf98265b7ca24b
Saad Ahmed <saamed9198@sbtjapan.com>
2023-03-14 11:46:47 +0500

    IF Statement

---
+++ js/14-if-statement/index.js
@@ -0,0 +1,20 @@
+// if statement = a basic form of decision making
+//                if a condition is true, the do something
+//                if not, then don't do it!
+
+let age = 65;
+
+if(age >= 65){
+    console.log("You are a senior citizen!")
+}
+else if(age >= 18){
+    console.log("You Are An Adult!")
+}
+else if (age < 0){
+    console.log("YOU HAVEN'T BEEN BORN YET!")
+}
+else{
+    console.log("You are a child");
+}
+
+let online = true;

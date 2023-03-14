834c6912c5654f1c414514c889d32c3c49570a6a
Saad Ahmed <saamed9198@sbtjapan.com>
2023-03-14 15:15:38 +0500

    LOOPS

---
+++ js/19-while-loop/index.js
@@ -0,0 +1,16 @@
+// while loop = repeate some code
+//              while some condition is true
+//              potentially infinite
+
+
+// let userName = "";
+
+// while(userName == "" || userName == null){
+//     userName = window.prompt("Enter Your Name");
+// }
+
+// console.log("Hello", userName);
+
+// while (1 == 1){
+//     console.log("Help! I'M STUCK IN INFINITE LOOP")
+// }
---
+++ js/20-do-while-loop/index.js
@@ -0,0 +1,24 @@
+// do while loop = do something,
+//                 then check the condition,
+//                 repeat if condition is true,
+
+
+// let userName = "";
+
+// userName = window.prompt("Enter your name");
+// do{
+//     userName = window.prompt("Enter your name");
+// }
+// while (userName == "")
+
+// console.log("Hellow", userName);
+
+
+// for loop = repeate some code a 
+//            certain amount of times
+
+for(let i = 10; i > 0; i-=3){
+    console.log(i)
+}
+
+console.log("Happy New Year!")

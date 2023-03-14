1a1b723b5795e9539d76d2c1cf5a7a15dda41305
Saad Ahmed <saamed9198@sbtjapan.com>
2023-03-14 16:34:20 +0500

    Var vs Let

---
+++ js/27-var-vs-let/index.js
@@ -0,0 +1,25 @@
+// variable scope = where a variable is accessible
+
+// let = variables are limited to a block scope
+// var = variable are limited to a function
+
+// global Variable = is declared outside any function
+
+// (if global, var will CHANGE browser's window properties)
+
+
+
+
+
+
+
+
+// doSomething()
+// function doSomething(){
+//     for (var i = 1; i <= 3; i+=1){
+//         // console.log(i);
+//     }
+// }
+
+
+// console.log(i); // Syntax error i is not defined

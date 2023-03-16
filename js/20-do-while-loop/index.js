6901e0bb2c171378b72045df44b1fc73529bf7a8
Saad Ahmed <saamed9198@sbtjapan.com>
2023-03-16 18:12:16 +0500

    Sort of an array

--- js/20-do-while-loop/index.js
+++ js/20-do-while-loop/index.js
@@ -3,22 +3,22 @@
 //                 repeat if condition is true,
 
 
-// let userName = "";
+let userName = "";
 
 // userName = window.prompt("Enter your name");
-// do{
-//     userName = window.prompt("Enter your name");
-// }
-// while (userName == "")
+do{
+    userName = window.prompt("Enter your name");
+}
+while (userName == "")
 
-// console.log("Hellow", userName);
+console.log("Hellow", userName);
 
 
 // for loop = repeate some code a 
 //            certain amount of times
 
-for(let i = 10; i > 0; i-=3){
-    console.log(i)
-}
+// for(let i = 10; i > 0; i-=3){
+//     console.log(i)
+// }
 
-console.log("Happy New Year!")
+// console.log("Happy New Year!")

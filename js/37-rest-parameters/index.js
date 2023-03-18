41c0de78d1917f91cfe620c46e924395b2022611
Saad Ahmed <devsaadahmed@gmail.com>
2023-03-18 19:21:41 +0500

    rest parameters

---
+++ js/37-rest-parameters/index.js
@@ -0,0 +1,33 @@
+// rest parameters = represents an indefinite number
+//                   of parameters
+//                   (packs arguments into an array)
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(sum(a,b,c));
+
+// function sum(a, b){
+//     return a + b;
+// }
+// function sum(a, b,c){
+//     return a + b + c;
+// }
+// function sum(a, b,c,d){
+//     return a + b + c + d;
+// }
+// function sum(a, b,c, d, e){
+//     return a + b + c + d + e;
+// }
+
+function sum(...numbers){
+    let total = 0;
+    for (let number of numbers){
+        total += number;
+    }
+    return total;
+}

51fea819ccda74633b2baf5a764128a0c48809cd
Saad Ahmed <devsaadahmed@gmail.com>
2023-03-18 16:42:39 +0500

    Sort array and reverse method

---
+++ js/34-sort-an-array-of-strings/index.js
@@ -0,0 +1,11 @@
+let fruits = ["banana","apple","orange","mango"];
+
+
+fruits = fruits.sort();   //  Make them arrange in order
+fruits = fruits.reverse();   // Make them reverse
+// fruits = fruits.sort().reverse();  // first sort in order according to alphatbets then rearrange them in reverse order
+// console.log(fruits);
+
+for (let fruit of fruits){
+    console.log(fruit);
+}

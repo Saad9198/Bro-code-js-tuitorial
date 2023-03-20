dd0fa1ca56296f784bb7c4e4a8abfb5d048c2c34
Saad Ahmed <saamed9198@sbtjapan.com>
2023-03-20 12:58:04 +0500

    call back function

---
+++ js/38-callback-function/index.js
@@ -0,0 +1,35 @@
+// callback = a function pass an argument
+//            to another function.
+
+//   Ensures that a function is not going 
+//   to run before a task completed. 
+//   Helps us develop asynchronous code 
+//   (When one function has to wait for another function) 
+//   that helps us avoid errors and potential problems
+//   Ex. Wait for a file to load 
+
+
+// let total = sum(2, 3);
+// displayConsole(total);
+// displayDOM(total);
+
+// function sum(x,y){
+//     let result = x + y;
+//     return result
+// }
+
+sum(2, 3,displayDOM);
+
+function sum(x, y, callback){
+    result = x + y;
+    callback(result);
+}
+
+
+
+function displayConsole(output){
+    console.log(output);
+}
+function displayDOM(output){
+    document.getElementById("myLabel").innerHTML = output;
+}
